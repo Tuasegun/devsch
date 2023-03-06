de2f570f84b0e5f62e2d5f1ffd66a800908b3d62
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-03-06 10:43:09 +0100

    fixed

--- constant/eventContent.tsx
+++ constant/eventContent.tsx
@@ -17,18 +17,18 @@ interface EventContent {
   
   export const eventContents: EventContent = {
     mainTitle: ' ',
-    subTitle: 'THE CHALLENGES JUNIOR DEVELOPERS FACE IN THE INDUSTRY',
+    subTitle: 'UPSKILING AND ESSENTIALS FOR JUNIOR DEVELOPERS',
     time: '7pm - 8pm (WAT)',
-    date: '4th March, 2023',
+    date: '11th March 2023',
     location: 'GOOGLE MEET',
     bannerImage: '/assets/images/event/speakerImage.jpeg',
     description:
-      "As technology continues to advance, the demand for software developers has increased, resulting in more junior developers joining the industry. ",
-    content1: "However, being a junior developer can be challenging due to various reasons such as lack of experience, imposter syndrome, and a steep learning curve. ",
-    content2: "These challenges can often make it difficult for junior developers to thrive in their roles and make progress in their careers. ",
-    content3: "In this session, we will be discussing the challenges that junior developers face in the industry with Adeola Olaleye, a Software Engineer with several years of experience. Join us on Saturday 4th March 2023 at 7pm-8pm to learn more about these challenges and how junior developers can overcome them.",
+      "In today's fast-paced and ever-changing tech industry, staying current with the latest technologies and skillsets is crucial for any developer, especially junior developers. Upskilling is the process of learning new skills or enhancing existing ones to stay relevant in the industry.",
+    content1: "In this session, we will be discussing the importance of upskilling for junior developers and the essential skills that every junior developer should possess.",
+    content2: "Our guest speaker, Balogun Micheal, a Software Engineer with several years of experience, will be sharing his insights on the topic. Join us on Saturday 11th March 2023 at 7pm-8pm to learn more about the importance of upskilling and the essential skills required for junior developers to succeed in the industry.",
+    content3: "",
     content4: "",
-    speakerName: 'Adeola Olaleye ',
-    speakerRole: 'Software Engineer ',
+    speakerName: 'Balogun Micheal',
+    speakerRole: 'Applications Specialist ',
   }
   
