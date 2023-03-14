2339384723f4a640156a6806d3edfafbbbe80a3e
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-03-14 21:35:06 +0100

    fixedTesti

--- constant/testimonialContents.tsx
+++ constant/testimonialContents.tsx
@@ -7,17 +7,17 @@ interface TestimonialContentInterface {
 export const TestimonialContent: TestimonialContentInterface[] = [
     {
         name: "Olakunle",
-        testimony: "Schfordevs has helped me build confidence in my coding skills. I learnt the most efficient and effective development process, soft skills needed to be a good frontend developer. I will miss my classmates and amazing tutor and mentors.",
+        testimony: "Schfordevs helped me build a strong confidence in coding skills, I learnt the most efficient and effective development process, soft skills needed to be a good frontend developer.",
         image: "assets/images/testimonial/testimonial1.png"
     },
     {
         name: "Tolu",
-        testimony: "Schfordevs provides the next generation of developers everything they need to succeed in a competitive Schfordevs provides the next generation of developers everything they need to succeed in a competitive Schfordevs provides the next generation of developers.",
+        testimony: "Really lovely and insightful experience. I learnt a lot during the program which would help me put all learnt skills to good use in the development field. Our Tutor really takes her time to explain each subject in detail over and over again till everyone understands.",
         image: "assets/images/testimonial/testimonial2.png"
     },
     {
         name: "Tolani",
-        testimony: "Schfordevs provides the next generation of developers everything they need to succeed in a competitive Schfordevs provides the next generation of developers everything they need to succeed in a competitive Schfordevs provides the next generation of developers.",
+        testimony: "My time at schfordevs was enlightening, fun and interesting. Learning from the tutors was really good and it's the best online class I have had.  I also met really interesting people who are fun to talk to and I have learned from them too.",
         image: "assets/images/testimonial/testimonial3.png"
     }
 
