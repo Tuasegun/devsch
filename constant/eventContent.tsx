b2e935f6320f22922933ecccd546ce2879393869
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-03-20 12:20:16 +0100

    fixed

--- constant/eventContent.tsx
+++ constant/eventContent.tsx
@@ -17,18 +17,18 @@ interface EventContent {
   
   export const eventContents: EventContent = {
     mainTitle: ' ',
-    subTitle: 'UPSKILING AND ESSENTIALS FOR JUNIOR DEVELOPERS',
+    subTitle: 'THE FRONTEND DEVELOPMENT ROADMAP',
     time: '7pm - 8pm (WAT)',
-    date: '11th March 2023',
+    date: '25th March 2023',
     location: 'GOOGLE MEET',
     bannerImage: '/assets/images/event/speakerImage.jpeg',
     description:
-      "In today's fast-paced and ever-changing tech industry, staying current with the latest technologies and skillsets is crucial for any developer, especially junior developers. Upskilling is the process of learning new skills or enhancing existing ones to stay relevant in the industry.",
-    content1: "In this session, we will be discussing the importance of upskilling for junior developers and the essential skills that every junior developer should possess.",
-    content2: "Our guest speaker, Balogun Micheal, a Software Engineer with several years of experience, will be sharing his insights on the topic. Join us on Saturday 11th March 2023 at 7pm-8pm to learn more about the importance of upskilling and the essential skills required for junior developers to succeed in the industry.",
+      " Frontend development is a critical aspect of software development that involves creating and designing the user interface (UI) and user experience (UX) of web applications. With the rapid advancement of technology and increased demand for user-friendly applications, frontend development has become an essential skill for software engineers to possess.",
+    content1: " However, for those just starting in this field, it can be challenging to navigate the learning process and understand the necessary skills required to excel in front-end development. In this session, we will be discussing the frontend development roadmap with Senpai Ada, a Technical Writer who has a lot of insights to share concerning this topic.",
+    content2: "Join us on Saturday 25th March 2023 from 7pm-8 pm to learn more about the skills and tools necessary to excel in frontend development and how to map out a successful career in this field.",
     content3: "",
     content4: "",
-    speakerName: 'Balogun Micheal',
-    speakerRole: 'Applications Specialist ',
+    speakerName: 'Senpai Ada',
+    speakerRole: 'Technical Writer',
   }
   
