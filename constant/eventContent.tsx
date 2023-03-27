df44b3459b9c7341a134bc0226d71638cb01cc7f
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-03-27 15:49:08 +0100

    nextevent

--- constant/eventContent.tsx
+++ constant/eventContent.tsx
@@ -17,18 +17,18 @@ interface EventContent {
   
   export const eventContents: EventContent = {
     mainTitle: ' ',
-    subTitle: 'THE FRONTEND DEVELOPMENT ROADMAP',
+    subTitle: 'CLEAN CODES AND BEST PRACTICES',
     time: '7pm - 8pm (WAT)',
-    date: '25th March 2023',
+    date: '1st April  2023',
     location: 'GOOGLE MEET',
     bannerImage: '/assets/images/event/speakerImage.jpeg',
     description:
-      " Frontend development is a critical aspect of software development that involves creating and designing the user interface (UI) and user experience (UX) of web applications. With the rapid advancement of technology and increased demand for user-friendly applications, frontend development has become an essential skill for software engineers to possess.",
-    content1: " However, for those just starting in this field, it can be challenging to navigate the learning process and understand the necessary skills required to excel in front-end development. In this session, we will be discussing the frontend development roadmap with Senpai Ada, a Technical Writer who has a lot of insights to share concerning this topic.",
-    content2: "Join us on Saturday 25th March 2023 from 7pm-8 pm to learn more about the skills and tools necessary to excel in frontend development and how to map out a successful career in this field.",
+      " Clean code and best practices are essential for any software development project, as they ensure that the code is easy to read, maintain, and understand. However, achieving clean code can be challenging, and it requires a good understanding of software design principles and coding best practices. In this session, Caleb Benjamin, a Frontend Engineer with several years of experience will discuss the importance of clean code and best practices in software development. ",
+    content1: "Join us on Saturday 1st April 2023 from 7 pm-8 pm to learn more about the benefits of clean code, coding best practices, and how to apply them to your software development projects.",
+    content2: "",
     content3: "",
     content4: "",
-    speakerName: 'Senpai Ada',
-    speakerRole: 'Technical Writer',
+    speakerName: 'Caleb Benjamin',
+    speakerRole: 'Frontend Engineer',
   }
   
