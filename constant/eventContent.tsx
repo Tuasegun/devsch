a71094b52b918b11c9cd86fdd403d193d7d0d4a6
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-05-16 11:52:04 +0100

    fixed

--- constant/eventContent.tsx
+++ constant/eventContent.tsx
@@ -17,18 +17,18 @@ interface EventContent {
   
   export const eventContents: EventContent = {
     mainTitle: ' ',
-    subTitle: 'THE ROLE OF ARTIFICIAL INTELLIGENCE IN WEB DEVELOPMENT',
+    subTitle: 'HOW TO BOOST YOUR PRODUCTIVITY AS A DEVELOPER',
     time: '7pm - 8pm (WAT)',
-    date: '13th May 2023',
+    date: '20th May 2023',
     location: 'GOOGLE MEET',
     bannerImage: '/assets/images/event/speakerImage.jpg',
     description:
-      "Artificial Intelligence (AI) and Machine Learning (ML) are becoming increasingly important in the field of web development. As the world becomes more connected, websites are being designed to provide more personalized experiences for users, and AI and ML are playing a key role in making this possible.",
-    content1: "In this session, we will be discussing the role of AI and ML in web development with Abdulrasaq Mustapha, a Software Engineer with extensive knowledge in this field. Join us on Saturday 13th of May 2023 from 7 pm-8 pm to learn more about the ways in which AI and ML are shaping the future of web development and how they can be integrated into your projects.",
-    content2: "",
+      "As a software developer, it's essential to be productive in order to meet project deadlines and accomplish tasks efficiently. However, productivity can be challenging to maintain, especially when faced with distractions or a lack of motivation.",
+    content1: "In this session, we will be discussing practical tips and strategies to help boost your productivity as a developer with Ebenezer Don, the founder of NewDev.io. We will cover topics such as time management, goal setting, optimizing your workspace, and maintaining focus.",
+    content2: "Join us on Saturday 20th May 2023 from 7 pm-8 pm to learn how to increase your productivity as a developer and reach your full potential.",
     content3: "",
     content4: "",
-    speakerName: 'Abdulrasaq Mustapha',
-    speakerRole: 'Frontend Engineer',
+    speakerName: 'Ebenezer Don',
+    speakerRole: 'Founder of NewDev.io',
   }
   
